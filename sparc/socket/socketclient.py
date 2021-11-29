774c7c076c24afdaac478e57c26a05a1061a0a84
Felix Wang <fwang356@gatech.edu>
2021-11-29 16:40:01 -0500

    Send data to server

--- sparc/socket/socketclient.py
+++ sparc/socket/socketclient.py
@@ -1,20 +1,15 @@
 import socket
-import json
-
-echo_sig = "ECHO "
-calc_sig = "ASE CALC "
+from ase import Atoms
 eor_sig = "\r\n\r\n"
 
 class SocketClient:
     
-    def __init__(self, sock_num:int=20801):
+    def __init__(self, sock_num:int=20801, atoms):
         self.sock_num = sock_num
         self.sock = None
+        self.atoms = atoms
 
     def connect(self):
-        """
-        
-        """
         print('Starting Client')
         print('Create a TCP/IP socket')
         self.sock = socket.socket(socket.AF_INET,
@@ -27,11 +22,7 @@ class SocketClient:
         self.sock.connect(server_address)
         print('Connected to server')
 
-
     def _send_message(self, message:str="test"):
-        """
-        
-        """
         if self.sock is None:
             raise TypeError("Must call `connect` before `send_message`.")
         try:
@@ -40,7 +31,6 @@ class SocketClient:
         except:
             print("There was an issue sending")
 
-
     def _recv_message(self):
         try:
             data = self.sock.recv(1024).decode()
@@ -48,16 +38,72 @@ class SocketClient:
         except:
             print("There was an issue recving")
 
-    def close(self):
-            print('Closing socket')
-            self.sock.close()
-
-    def echo_message(self, message:str="test"):
-        self._send_message(echo_sig + message + eor_sig)
+    def echo(self, message:str="test"):
+        self._send_message("ECHO" + eor_sig)
+        self._send_message(message + eor_sig)
         self._recv_message()
 
-    def send_calc_request(self, grid_spacing, atom, cubic):
-        # ["ASE CALC", 0.2, "Si", True]
-        params = [calc_sig, grid_spacing, atom, cubic]
-        self._send_message(json.dumps(params).encode())
+    def abort(self):
+        print('Instructing server to shut down.')
+        self._send_message("ABORT" + eor_sig)
+        print('Closing socket.')
+        self.sock.close()
+
+    def status(self):
+        print("getting status")
+        self._send_message("STATUS" + eor_sig)
         self._recv_message()
+
+    def init(self, bead_index=1, init_str="hello"):
+        print("initializing")
+        self._send_message("INIT" + eor_sig)
+        self._send_message(str(bead_index).encode('utf8') + eor_sig)
+        init_str_len = len(init_str)
+        self._send_message(str(init_str_len).encode('utf8') + eor_sig)
+        if init_str_len > 0:
+            self._send_message(init_str + eor_sig)
+
+    def posdata(self):
+        print("sending data")
+        self._send_message("POSDATA" + eor_sig)
+        self._send_message(self.get_matrix.encode('utf8') + eor_sig)
+        self._send_message(self.get_num_atoms.encode('utf8') + eor_sig)
+        self._send_message(self.get_coords.encode('utr8') + eor_sig)
+
+    def getforce(self):
+        print("getting force")
+        self._send_message("GETFORCE" + eor_sig)
+        reply = ""
+        while True:
+            try:
+                reply = self.recv()
+            except:
+                print("Error while receiving message: &s" % reply)
+            if reply == "FORCEREADY":
+                break
+        
+        if reply != "":
+            pot = np.float64()
+            pot = self.recv()
+
+            mlen = np.int32()
+            mlen = self.recv()
+
+            force = np.zeros(3 * mlen, np.float64)
+            force = self.recv()
+
+            vir = np.zeros((3, 3), np.float64)
+            vir = self.recv()
+
+            return [pot, force, vir]
+        
+    def get_coords(self):
+        matrix = self.atoms.get_positions()
+        return ','.join(str(item) for innerlist in matrix for item in innerlist)
+
+    def get_matrix(self):
+        matrix = self.atoms.cell[:]
+        return ','.join(str(item) for innerlist in matrix for item in innerlist)
+
+    def get_num_atoms(self):
+        return str(len(self.atoms))
